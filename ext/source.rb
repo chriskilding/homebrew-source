6a1ba15027e414216eeb166bf34ae0de6d671f86
Chris Kilding <590569+chriskilding@users.noreply.github.com>
2022-07-28 18:09:32 +0100

    Add minimum working example of command from docs

---
+++ ext/source.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Homebrew
+  module_function
+
+  def source_args
+    Homebrew::CLI::Parser.new do
+      description <<~EOS
+        Automatically 'source' shell functions from formulae in your shell profile
+      EOS
+      switch "-f", "--force",
+             description: "Force doing something in the command."
+      flag   "--file=",
+             description: "Specify a file to do something with in the command."
+      comma_array "--names",
+                  description: "Add a list of names to the command."
+
+      named_args [:formula, :cask], min: 1
+    end
+  end
+
+  def source
+    args = foo_args.parse
+
+    something if args.force?
+    something_else if args.file == "file.txt"
+  end
+end
