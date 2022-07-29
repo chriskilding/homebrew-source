037214b7317704d1ce18fac5393c6b739ea1ed07
Chris Kilding <590569+chriskilding@users.noreply.github.com>
2022-07-29 15:55:28 +0100

    Refactor out 'missing source directives' detection to method

--- cmd/source.rb
+++ cmd/source.rb
@@ -5,67 +5,61 @@ require "cli/parser"
 require "formula"
 
 module Homebrew
-  extend T::Sig
-  
-  module_function
+    extend T::Sig
 
-  sig { returns(CLI::Parser) }
-  def source_args
-    Homebrew::CLI::Parser.new do
-      description <<~EOS
-        Automatically 'source' shell functions from formulae in your shell profile
-      EOS
+    module_function
 
-      named_args [:formula], min: 1, max: 1
-    end
-  end
+    sig { returns(CLI::Parser) }
+    def source_args
+        Homebrew::CLI::Parser.new do
+            description <<~EOS
+                Automatically 'source' shell functions from formulae in your shell profile
+            EOS
 
-  sig { void }
-  def source
-    args = source_args.parse
-    
-    # Keep this after the .parse to keep --help fast.
-    require_relative "../lib/source"
-
-    formula_name = args.named.first
-    formula = Formulary.factory(formula_name)
-
-    puts "Sourcing the shell functions from '#{formula}' in your #{shell_profile}..."
-
-    # FIXME read from the formula
-    files_to_source = [
-      "/usr/local/share/zsh/site-functions/#{formula.name}",
-      "/usr/local/share/zsh/site-functions/#{formula.name}-foo"
-    ]
-
-    if files_to_source.empty?
-      puts "...this formula does not contain shell functions, so no action was taken."
-      return
-    end
-
-    shell_profile_path = File.expand_path(shell_profile)
-
-    File.open(shell_profile_path, 'r') do |file|
-      file.each do |line|
-        files_to_source.each do |file_to_source|
-          if Source::ShellProfile.includes_source_directive?(line, file_to_source)
-            files_to_source.delete file_to_source
-          end
+            named_args [:formula], min: 1, max: 1
         end
-      end
     end
 
-    if files_to_source.empty?
-      puts "...this formula's shell functions are already sourced, so no action was taken."
-      return
-    end
+    sig { void }
+    def source
+        args = source_args.parse
+        
+        # Keep this after the .parse to keep --help fast.
+        require_relative "../lib/source"
 
-    File.open(shell_profile_path, 'a') do |file|
-      files_to_source.each do |file_to_source|
-        file.write("source #{file_to_source}\n")
-      end
+        formula_name = args.named.first
+        formula = Formulary.factory(formula_name)
 
-      puts "...done."
+        puts "Sourcing the shell functions from '#{formula}' in your #{shell_profile}..."
+
+        # FIXME read from the formula
+        files_to_source = [
+            "/usr/local/share/zsh/site-functions/#{formula.name}",
+            "/usr/local/share/zsh/site-functions/#{formula.name}-foo"
+        ]
+
+        if files_to_source.empty?
+            puts "...this formula does not contain shell functions, so no action was taken."
+            return
+        end
+
+        shell_profile_path = File.expand_path(shell_profile)
+        missing_directives = []
+        File.open(shell_profile_path, 'r') do |file|
+            missing_directives = Source::ShellProfile.missing_source_directives(file, files_to_source)
+        end
+
+        if missing_directives.empty?
+            puts "...this formula's shell functions are already sourced, so no action was taken."
+            return
+        end
+
+        File.open(shell_profile_path, 'a') do |file|
+            missing_directives.each do |file_to_source|
+                file.write("source #{file_to_source}\n")
+            end
+        end
+        
+        puts "...done."
     end
-  end
 end
--- lib/source/shell_profile.rb
+++ lib/source/shell_profile.rb
@@ -6,9 +6,27 @@ module Source
     class ShellProfile
 
         ##
-        # Determine whether the shell profile contains a 'source' directive for the specified shell functions script
-        def self.includes_source_directive?(rc, shell_functions_script)
-            rc =~ /^[^#]*(source|\.)\s+#{shell_functions_script}(#.*|\s.*|\b)$/
+        # Determine whether str contains a 'source' directive for the specified shell functions script
+        def self.includes_source_directive?(str, shell_functions_script)
+            str =~ /^[^#]*(source|\.)\s+#{shell_functions_script}(#.*|\s.*|\b)$/
+        end
+
+        ##
+        # Scan the provided rc file for the expected 'source' directives, and output which ones are missing from it.
+        def self.missing_source_directives(rc, files_to_source=[])
+            # 1. Convert to set (to ensure element uniqueness)
+            # 2. Ensures data is copied (to avoid mutating the original)
+            missing_directives = files_to_source.to_set
+
+            rc.each do |line|
+                missing_directives.each do |directive|
+                    if Source::ShellProfile.includes_source_directive?(line, directive)
+                        missing_directives.delete directive
+                    end
+                end
+            end
+
+            missing_directives
         end
     end
 end
